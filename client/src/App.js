c142522c43a18a35efb109f6dd71237274ab602d
Pellaton Yves <yves.pellaton@bbcag.ch>
2021-09-03 17:59:20 +0200

    immprove setup, add fetch from backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,36 @@
 import logo from './img/logo.png';
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 function App() {
-  useEffect(() => {
-    const fetchData = async () => {
-      
-      const json = await fetch("/express_backend").then(r =>r.json()).catch(e => console.error(e))
-      console.log(json)
-    }
 
-    fetchData()
-  }, [])
+    const [state, setState] = useState("noene")
+    const [show, setShow] = useState(false)
 
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        fetch("/express_backend")
+            .then(r => r.json())
+            .then(data => {
+                setState(data.express)
+                setShow(true)
+            })
+            .catch(e => console.error(e))
+
+    }, [])
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                {show ? <h2> Loook {state}</h2> : <p>None yet</p>}
+            </header>
+        </div>
+    );
 }
 
 export default App;
