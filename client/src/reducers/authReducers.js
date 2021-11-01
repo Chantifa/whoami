75de0f9171de5a904c1cbc1b2ef6997e215854a4
Inetphantom <git@inetphantom.ch>
2021-11-01 10:49:38 +0100

    use es6 instead of commonjs for authReducers.js

--- client/src/reducers/authReducers.js
+++ client/src/reducers/authReducers.js
@@ -1,6 +1,5 @@
 import {SET_CURRENT_USER, USER_LOADING} from "../actions/types";
-
-const isEmpty = require("is-empty");
+import * as isEmpty from "is-empty"
 
 const initialState = {
     isAuthenticated: false,
