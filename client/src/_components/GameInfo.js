58dccc3dc9d1d185d097158e77ee5865cebc0e6d
Yves B. Pellaton <yves.pellaton@students.ffhs.ch>
2021-11-05 17:12:58 +0100

    improve user state display

--- client/src/_components/GameInfo.js
+++ client/src/_components/GameInfo.js
@@ -2,24 +2,34 @@ import {Col, Row, Table} from "react-bootstrap";
 
 
 export default function GameInfo(props) {
-    console.log(props)
     if (!props.info || !props.info.personaMapInPlayOrder) {
         return null
     }
     const people = [...props.info.personaMapInPlayOrder]
 
     function PersonaMapTableBody(innerProps) {
-        console.log(innerProps)
         const user = innerProps.value[0]
         const alias = innerProps.value[1]
         const isOnTurn = (props.state && props.state.currentUser.userId === user.userId)
         const calculatedClassNames = isOnTurn ? "table-info" : ""
-        const voteInfo = "not yet done"
+
+        let playInfo
+        if (!props.state || !props.state.votes) {
+            playInfo = null
+        } else if (isOnTurn) {
+            playInfo = props.state.currentQuestion ? props.state.currentQuestion : "thinking of a question"
+        } else if (!props.state.currentQuestion) {
+            playInfo = "waiting for a question"
+        } else if (props.state.votes.length === 0) {
+            playInfo = "voting.."
+        } else {
+            playInfo = new Map(props.state.votes).get(user.userId) ? "yeap" : "nooop"
+        }
 
         return <tr className={calculatedClassNames}>
             <td>{user.userName}</td>
             <td>{alias}</td>
-            {props.state ? <td>{voteInfo}</td> : null}
+            {props.state ? <td>{playInfo}</td> : null}
         < /tr>;
     }
 
@@ -32,11 +42,11 @@ export default function GameInfo(props) {
                 <tr>
                     <th>User</th>
                     <th>Persona</th>
-                    {props.state ? <th>Vote info</th> : null}
+                    {props.state ? <th>Play info</th> : null}
                 </tr>
                 </thead>
                 <tbody>
-                {people.map((value, key) => <PersonaMapTableBody key={key} value={value}/> )}
+                {people.map((value, key) => <PersonaMapTableBody key={key} value={value}/>)}
 
                 </tbody>
             </Table>
