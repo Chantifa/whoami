10e37ec88b4260368830b7a6b66f057e2be5147f
ramona.koksa@students.ffhs.ch <ramona.koksa@students.ffhs.ch>
2021-11-14 13:03:06 +0100

    add page structure and panels

--- client/src/_components/Game.js
+++ client/src/_components/Game.js
@@ -1,9 +1,15 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import {useParams} from "react-router-dom/cjs/react-router-dom";
 import useServer from "../serverConnection";
-import {Button, Col, Form, Row} from "react-bootstrap";
 import Chat from "./Chat";
 import GameInfo from "./GameInfo";
+import {
+    Button,
+    Container,
+    Row,
+    Col
+} from "reactstrap";
+import {Form} from "react-bootstrap";
 
 export default function Game(props) {
 
@@ -11,12 +17,12 @@ export default function Game(props) {
     const [chatText, setChatChatText] = useState("Hello");
     const [question, setQuestion] = useState("Who am I?")
 
+    let pageHeader = React.createRef();
+
     const handleChatTextChange = (event) => setChatChatText(event.target.value);
     const handleQuestionChange = (event) => setQuestion(event.target.value);
 
     const {sendMessage, messageList, gameInfo, gameState, sendVote, sendQuestion, startGame } = useServer(props.userName, id)
-
-
 
     function handleChatSubmit(event) {
         event.preventDefault()
@@ -30,53 +36,93 @@ export default function Game(props) {
         setQuestion("")
     }
 
-
     return <>
-        <main className="container grid" style={{paddingTop: 110 + 'px'}}>
-            <Row>
-                <h1> Game {id} </h1>
-            </Row>
-
             <code>{JSON.stringify(gameState)}</code>
+            <div
+                style={{
+                    backgroundImage:
+                        "url(" + require("../assets/img/galaxy.jpg").default + ")",
+                }}
+                className="page-header page-header-xs"
+                data-parallax={true}
+                ref={pageHeader}
+            >
+                <div className="filter"/>
+            </div>
+            <div className="section profile-content">
+                <Container>
+                    <div className="owner">
+                        <div className="avatar">
+                            <img
+                                alt="..."
+                                className="img-circle img-no-padding img-responsive"
+                                src={require("../assets/img/mobile.png").default}
+                                border={"1px solid black"}
+                            />
+                        </div>
+                        <div className="name">
+                            <h4 className="title">Game {id}<br/>
+                            </h4>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+            <div className="main">
+                <Container>
+                    <Row>
+                        <Col>
+                            <GameInfo info={gameInfo} state={gameState}/>
 
-            <Row>
+                            <Button outline className="btn-round ml-1 btn btn-outline-success"
+                                    data-toggle="tooltip"
+                                    title="Select this button when the person has been found out."
+                                    onClick={sendVote.bind(null, true)}> Yeap!
+                                <i className="fa fa-heart mr-1"/>
+                            </Button>{' '}
+                            <Button className="btn-round ml-1 btn btn-outline-danger"
+                                    data-toggle="tooltip"
+                                    title="Select this button when the person has NOT been found out."
+                                    onClick={sendVote.bind(null, false)}> Nope!
+                                <i className="nc-icon nc-simple-remove mr-1"/>
+                            </Button>{' '}
+                            <Button className="btn-round ml-1 btn btn-outline-primary"
+                                    data-toggle="tooltip"
+                                    title="Select this button to start a game."
+                                    onClick={startGame}> start
+                                <i className="nc-icon nc-button-play mr-1"/>
+                            </Button>
 
-                <Col>
+                            <Form className="contact-form" onSubmit={handleQuestionSubmit}>
+                                <Form.Group>
+                                    <Form.Control type="text" placeholder="Enter your question"
+                                                  onChange={handleQuestionChange}
+                                                  value={question}/>
 
-                    <GameInfo info={gameInfo} state={gameState}/>
+                                    <Form.Text className="text-muted">
+                                        Your question will be queued until it's your turn
+                                    </Form.Text>
+                                </Form.Group>
+                                <Button type="submit">Ask Question</Button>
+                            </Form>
+                        </Col>
+                        <Col>
+                            <Chat messages={messageList}/>
+                            <Form onSubmit={handleChatSubmit}>
+                                <Form.Group>
+                                    <Form.Control required type="text" placeholder="Enter your message"
+                                                  onChange={handleChatTextChange}
+                                                  value={chatText}/>
+                                    <Form.Text className="text-muted">
+                                        You should not ask questions here, this is only the chat
+                                    </Form.Text>
+                                </Form.Group>
+                                <Button type="submit">Send Message</Button>
+                            </Form>
 
-                    <Button className="btn-outline-success" onClick={sendVote.bind(null, true)}> Yeap! </Button>
-                    <Button className="btn-outline-danger" onClick={sendVote.bind(null, false)}> Nope! </Button>
-                    <Button onClick={startGame}> start </Button>
+                        </Col>
 
-                    <Form onSubmit={handleQuestionSubmit}>
-                        <Form.Group>
-                            <Form.Control type="text" placeholder="Enter your question" onChange={handleQuestionChange}
-                                          value={question}/>
-                            <Form.Text className="text-muted">
-                                Your question will be queued until it's your turn
-                            </Form.Text>
-                        </Form.Group>
-                        <Button type="submit">Ask Question</Button>
-                    </Form>
-
-                </Col>
-                <Col>
-                    <Chat messages={messageList}/>
-
-                    <Form onSubmit={handleChatSubmit}>
-                        <Form.Group>
-                            <Form.Control required type="text" placeholder="Enter your message"
-                                          onChange={handleChatTextChange}
-                                          value={chatText}/>
-                            <Form.Text className="text-muted">
-                                You should not ask questions here, this is only the chat
-                            </Form.Text>
-                        </Form.Group>
-                        <Button type="submit">Send Message</Button>
-                    </Form>
-                </Col>
-            </Row>
-        </main>
-    </>;
+                    </Row>
+                </Container>
+            </div>
+        </>;
 }
