49935d865810f6304b15b34c97870e8cb7312dd9
ramona.koksa@students.ffhs.ch <ramona.koksa@students.ffhs.ch>
2021-11-15 09:23:14 +0100

    add padding and border on left panel

--- client/src/_components/Game.js
+++ client/src/_components/Game.js
@@ -22,7 +22,15 @@ export default function Game(props) {
     const handleChatTextChange = (event) => setChatChatText(event.target.value);
     const handleQuestionChange = (event) => setQuestion(event.target.value);
 
-    const {sendMessage, messageList, gameInfo, gameState, sendVote, sendQuestion, startGame } = useServer(props.userName, id)
+    const {
+        sendMessage,
+        messageList,
+        gameInfo,
+        gameState,
+        sendVote,
+        sendQuestion,
+        startGame
+    } = useServer(props.userName, id)
 
     function handleChatSubmit(event) {
         event.preventDefault()
@@ -37,90 +45,89 @@ export default function Game(props) {
     }
 
     return <>
-            <code>{JSON.stringify(gameState)}</code>
-            <div
-                style={{
-                    backgroundImage:
-                        "url(" + require("../assets/img/galaxy.jpg").default + ")",
-                }}
-                className="page-header page-header-xs"
-                data-parallax={true}
-                ref={pageHeader}
-            >
-                <div className="filter"/>
-            </div>
-            <div className="section profile-content">
-                <Container>
-                    <div className="owner">
-                        <div className="avatar">
-                            <img
-                                alt="..."
-                                className="img-circle img-no-padding img-responsive"
-                                src={require("../assets/img/mobile.png").default}
-                                border={"1px solid black"}
-                            />
-                        </div>
-                        <div className="name">
-                            <h4 className="title">Game {id}<br/>
-                            </h4>
-                        </div>
+        <code>{JSON.stringify(gameState)}</code>
+        <div
+            style={{
+                backgroundImage:
+                    "url(" + require("../assets/img/galaxy.jpg").default + ")",
+            }}
+            className="page-header page-header-xs"
+            data-parallax={true}
+            ref={pageHeader}
+        >
+            <div className="filter"/>
+        </div>
+        <div className="section profile-content">
+            <Container>
+                <div className="owner">
+                    <div className="avatar">
+                        <img
+                            alt="..."
+                            className="img-circle img-no-padding img-responsive"
+                            src={require("../assets/img/mobile.png").default}
+                            border={"1px solid black"}
+                        />
                     </div>
-                </Container>
+                    <div className="name">
+                        <h4 className="title">Game {id}<br/>
+                        </h4>
+                    </div>
+                </div>
+            </Container>
             </div>
-            <div className="main">
-                <Container>
-                    <Row>
-                        <Col>
-                            <GameInfo info={gameInfo} state={gameState}/>
+        <Container fluid>
+            <Row>
+                <Col className="square border border-1 border-dark rounded-3 ms-xl-5 p-3">
+                    <GameInfo info={gameInfo} state={gameState}/>
 
-                            <Button outline className="btn-round ml-1 btn btn-outline-success"
-                                    data-toggle="tooltip"
-                                    title="Select this button when the person has been found out."
-                                    onClick={sendVote.bind(null, true)}> Yeap!
-                                <i className="fa fa-heart mr-1"/>
-                            </Button>{' '}
-                            <Button className="btn-round ml-1 btn btn-outline-danger"
-                                    data-toggle="tooltip"
-                                    title="Select this button when the person has NOT been found out."
-                                    onClick={sendVote.bind(null, false)}> Nope!
-                                <i className="nc-icon nc-simple-remove mr-1"/>
-                            </Button>{' '}
-                            <Button className="btn-round ml-1 btn btn-outline-primary"
-                                    data-toggle="tooltip"
-                                    title="Select this button to start a game."
-                                    onClick={startGame}> start
-                                <i className="nc-icon nc-button-play mr-1"/>
-                            </Button>
+                    <Button outline className="btn-round ml-1 btn btn-outline-success"
+                            data-toggle="tooltip"
+                            title="Select this button when the person has been found out."
+                            onClick={sendVote.bind(null, true)}> Yeap!
+                        <i className="fa fa-heart mr-1"/>
+                    </Button>{' '}
+                    <Button className="btn-round ml-1 btn btn-outline-danger"
+                            data-toggle="tooltip"
+                            title="Select this button when the person has NOT been found out."
+                            onClick={sendVote.bind(null, false)}> Nope!
+                        <i className="nc-icon nc-simple-remove mr-1"/>
+                    </Button>{' '}
+                    <Button className="btn-round ml-1 btn btn-outline-primary"
+                            data-toggle="tooltip"
+                            title="Select this button to start a game."
+                            onClick={startGame}> start
+                        <i className="nc-icon nc-button-play mr-1"/>
+                    </Button>
 
-                            <Form className="contact-form" onSubmit={handleQuestionSubmit}>
-                                <Form.Group>
-                                    <Form.Control type="text" placeholder="Enter your question"
-                                                  onChange={handleQuestionChange}
-                                                  value={question}/>
+                    <Form className="contact-form" onSubmit={handleQuestionSubmit}>
+                        <Form.Group>
+                            <Form.Control type="text" placeholder="Enter your question"
+                                          onChange={handleQuestionChange}
+                                          value={question}/>
 
-                                    <Form.Text className="text-muted">
-                                        Your question will be queued until it's your turn
-                                    </Form.Text>
-                                </Form.Group>
-                                <Button type="submit">Ask Question</Button>
-                            </Form>
-                        </Col>
-                        <Col>
-                            <Chat messages={messageList}/>
-                            <Form onSubmit={handleChatSubmit}>
-                                <Form.Group>
-                                    <Form.Control required type="text" placeholder="Enter your message"
-                                                  onChange={handleChatTextChange}
-                                                  value={chatText}/>
-                                    <Form.Text className="text-muted">
-                                        You should not ask questions here, this is only the chat
-                                    </Form.Text>
-                                </Form.Group>
-                                <Button type="submit">Send Message</Button>
-                            </Form>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        </>;
+                            <Form.Text className="text-muted">
+                                Your question will be queued until it's your turn
+                            </Form.Text>
+                        </Form.Group>
+                        <Button className="btn-round btn-warning text-black" type="submit">Ask Question</Button>
+                        <div className="margin-top"/>
+                    </Form>
+                </Col>
+                <Col className="p-5 ms-xl-1">
+                    <Chat messages={messageList}/>
+                    <Form onSubmit={handleChatSubmit}>
+                        <Form.Group>
+                            <Form.Control required type="text" placeholder="Enter your message"
+                                          onChange={handleChatTextChange}
+                                          value={chatText}/>
+                            <Form.Text className="text-muted">
+                                You should not ask questions here, this is only the chat
+                            </Form.Text>
+                        </Form.Group>
+                        <Button type="submit">Send Message</Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    </>;
 }
