75c5a09709f5c048a30b933463c4945443c2db32
Inetphantom <git@inetphantom.ch>
2021-11-16 22:07:13 +0100

    don't load bootstraps scss but only paper-kit scss

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import {App} from './App';
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/scss/bootstrap.scss";
 import "./assets/scss/paper-kit.scss?v=1.3.0";
 import "./assets/demo/demo.css?v=1.3.0";
 import { Provider } from 'react-redux';
