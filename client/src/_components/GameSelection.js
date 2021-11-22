d57e65c1c45e84d6b5de132cb9ba4ec32cd06742
Inetphantom <git@inetphantom.ch>
2021-11-22 12:02:58 +0100

    Add room selection by name, start overview

--- client/src/_components/GameSelection.js
+++ client/src/_components/GameSelection.js
@@ -1,35 +1,67 @@
 import {Route} from "react-router-dom";
 import {useRouteMatch} from "react-router-dom/cjs/react-router-dom";
-import {useContext, useEffect, useState} from 'react';
+import {createRef, useContext, useEffect, useState} from 'react';
 import Game from "./Game";
 import {ReactReduxContext} from "react-redux";
-import {ListGroup, ListGroupItem, NavLink} from "reactstrap";
+import {Button, Input, ListGroup} from "reactstrap";
+
+function GameSelectionItem(props) {
+    return <code>{JSON.stringify(props.game)} </code>
+}
 
 export default function GameSelection() {
 
+    function getRandId() {
+        const ID_LENGTH = 8
+        return String(Math.floor(Math.random() * 10 ** ID_LENGTH)).padStart(ID_LENGTH, '0')
+    }
+
     const [user, setUser] = useState("")
+    const [games, setGames] = useState([])
+    const [refresh, setRefresh] = useState(0)
+    const [selectedGame, setSelectedGame] = useState(getRandId())
     const {store} = useContext(ReactReduxContext);
+
+    let pageHeader = createRef();
 
     useEffect(() => {
         setUser(store.getState().authentication.user.message.name)
     }, [store])
 
+    useEffect(() => {
+        fetch("/api/games")
+            .then(response => response.json())
+            .then(json => setGames(json))
+            .catch(e => alert(e.message)) //fixme alerts
+    }, [refresh])
+
     let {path, url} = useRouteMatch();
+
+    const handleSelectedChange = (event) => setSelectedGame(event.target.value);
 
     return (
         <>
+            <div
+                style={{
+                    backgroundImage:
+                        "url(" + require("../assets/img/galaxy.jpg").default + ")",
+                }}
+                className="page-header page-header-xs"
+                data-parallax={true}
+                ref={pageHeader}
+            >
+                <div className="filter"/>
+            </div>
             <Route path={`${path}/:id`}>
                 <Game userName={user}/></Route>
             <Route exact path={path}>
+                <h1>Game Overview</h1>
                 <ListGroup>
-                    {/*todo get this list from fetch call*/}
-                    <ListGroupItem><NavLink href={url + "/7"}> 7</NavLink> </ListGroupItem>
-                    <ListGroupItem><NavLink href={url + "/6"}> 6</NavLink> </ListGroupItem>
-                    <ListGroupItem><NavLink href={url + "/5"}> 5</NavLink> </ListGroupItem>
-                    <ListGroupItem><NavLink href={url + "/4"}> 4</NavLink> </ListGroupItem>
-                    <ListGroupItem><NavLink href={url + "/3"}> 3</NavLink> </ListGroupItem>
-                    <ListGroupItem><NavLink href={url + "/2"}> 2</NavLink> </ListGroupItem>
+                    {games.map((value, key) => <GameSelectionItem key={key} game={value}/>)}
                 </ListGroup>
+                <Button onClick={setRefresh.bind(null, refresh + 1)}>Refresh</Button>
+                <Input type="text" maxLength="12" minLength="4" value={selectedGame} onChange={handleSelectedChange}/>
+                <a href={"/game/" + selectedGame} className="btn btn-success"> Join Room</a>
             </Route>
         </>
     )
