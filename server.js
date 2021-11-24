d8be74809f6544a43fea92ff40cd0a8133fa6ab0
Pellaton Yves <yves.pellaton@students.ffhs.ch>
2021-11-24 14:28:59 +0100

    improve http codes, add highscore/overview

--- server.js
+++ server.js
@@ -196,18 +196,41 @@ app.get('/api/games', (req, res) => {
     res.send(getOverview());
 });
 
-app.get('/api/user/:userId/info', (req, res) => {
+app.get('/api/userInfo/:userId', (req, res) => {
     const userId = req.params.userId
 
-    UserInfo.findOne({'userId': userId})
-        .then(userInfo => {
-            if (!userInfo){
-                res.status(404).json({userInfo: "Not found"})
-            } else {
-                res.status(200).json(userInfo)
-            }
-        })
-        .catch(error => console.log(error, error.message))
+    if (userId.toLowerCase() === "highscore" || userId === "") {
+        UserInfo.find({}, ["username", "userId","gamesStarted", "gamesFinished", "gamesWon" ])
+            .sort({ "gamesWon" : -1}).limit(10)
+            .exec()
+            .then(highscore => {
+                if (!highscore) {
+                    res.status(404).json({userInfo: "Not found"})
+                } else {
+                    res.status(200).json(highscore)
+                }
+            })
+            .catch(error => {
+                console.log(error, error.message)
+                res.status(500)
+            })
+
+    } else {
+        UserInfo.findOne({'userId': userId})
+            .then(userInfo => {
+                if (!userInfo) {
+                    res.status(404).json({userInfo: "Not found"})
+                } else {
+                    res.status(200).json(userInfo)
+                }
+            })
+            .catch(error => {
+                console.log(error, error.message)
+                res.status(500)
+            })
+
+    }
+
 
 })
 
